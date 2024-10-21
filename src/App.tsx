cbb0ee168f9164a99530365ad79fcee7d1214a8b
tarunsingh32 <128855711+tarunsingh32@users.noreply.github.com>
2024-10-21 14:44:58 +0530

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Rusable component/Home";
 import AllEvent from "./components/Admin/AllEvent";
+import Register from "./components/Auth/Register";
+import Login from "./components/Auth/Login";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+
+
         <Route path="/allEvents" element={<AllEvent />}></Route>
       </Routes>
     </BrowserRouter>
