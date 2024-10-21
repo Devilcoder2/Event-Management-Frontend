2198a8a5b8ae9a30e2d4c757046a837a2586e510
vishalmapsense <210108072@hbtu.ac.in>
2024-10-21 13:01:58 +0530

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-    return <div className="bg-red-600">App</div>;
+  return <div className="bg-red-600">
+    
+    
+        
+    
+    
+    </div>;
 };
 
 export default App;
