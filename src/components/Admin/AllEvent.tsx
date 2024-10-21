9b14b92907a1e1ef0bf83e8e9fe3679b3d1f3d1e
vishalmapsense <210108072@hbtu.ac.in>
2024-10-21 14:48:06 +0530

    add cards

--- src/components/Admin/AllEvent.tsx
+++ src/components/Admin/AllEvent.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Card from '../Rusable component/Card';
 
 const AllEvent = () => {
   return (
     <div>
-      <h2 className="text-xl font-bold mb-4">All Users</h2>
-      <p>List of all users will be displayed here.</p>
+      <h2 className="text-xl font-bold mb-4">All Events</h2>
+     <div className='flex gap-5'>
+     <Card/>
+     <Card/>
+     <Card/>
+     </div>
     </div>
   );
 };
